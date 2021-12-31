7a96f84e39edde9363d52d43d93a04f63b4e1f39
Brent Aronsen <bkaronsen@gmail.com>
2021-12-31 17:16:13 -0600

    Use react-router-transition

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import './Modal.css';
+import "./Modal.css";
 
 const modal = (props) => {
-    const cssClasses = ['Modal', props.show ? 'ModalOpen' : 'ModalClose'];
+  const cssClasses = [
+    "Modal",
+    props.show === "entering" ? "ModalOpen" 
+    : props.show === 'exiting' ? "ModalClose" : null,
+  ];
 
-    return (
-        <div className={cssClasses.join(' ')}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>Dismiss</button>
-        </div>
-    )
+  return (
+    <div className={cssClasses.join(" ")}>
+      <h1>A Modal</h1>
+      <button className="Button" onClick={props.closed}>
+        Dismiss
+      </button>
+    </div>
+  );
 };
 
 export default modal;
